b50883812a081993d21e2a1bbf982fbe2d20e4c7
Draizel <draizel16@example.com>
2025-10-02 19:35:57 +0800

    feat: add Password Generator logic

--- app/src/main/java/com/example/cybersecurityapp/ui/PasswordGeneratorActivity.kt
+++ app/src/main/java/com/example/cybersecurityapp/ui/PasswordGeneratorActivity.kt
@@ -1,13 +1,24 @@
 package com.example.cybersecurityapp.ui
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cybersecurityapp.R
+import kotlin.random.Random
 
 class PasswordGeneratorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_password_generator)
-        title = "Password Generator"
+
+        val edtPassword = findViewById<EditText>(R.id.edtPassword)
+        val btnGenerate = findViewById<Button>(R.id.btnGenerate)
+
+        btnGenerate.setOnClickListener {
+            val chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#\$%^&*()"
+            val password = (1..12).map { chars.random() }.joinToString("")
+            edtPassword.setText(password)
+        }
     }
 }
